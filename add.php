56218f619a787371e735c9005ea69fc09b1547f0
ahmadfebri14 <ahmadfebri14@gmail.com>
2020-01-25 21:41:27 +0700

    fix error2

--- add.php
+++ add.php
@@ -1,5 +1,5 @@
 <?php
-	if($_GET['act'] == "add"){	
+	if($_GET['act'] == "add"){
 		/*Insert data.*/  
         $insertSql = "INSERT INTO empTable (name, country, city, email)   
 					VALUES (?,?,?,?)";  
@@ -7,10 +7,18 @@
         );  
         $stmt = sqlsrv_query($conn, $insertSql, $params);  
         if ($stmt === false) {  
+            /*Handle the case of a duplicte e-mail address.*/  
+            $errors = sqlsrv_errors();  
+            if ($errors[0]['code'] == 2601)  
+                {  
+                echo "The e-mail address you entered has already been used.</br>";  
+                }  
+  
             /*Die if other errors occurred.*/  
-             
+              else  
+                {  
                 die(print_r($errors, true));  
-                
+                }  
             }else{  
 				echo '<script> alert("Data tersimpan"); window.location.href="./index.php"; </scr';
             }    
