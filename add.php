7c3d2c131ea766f5a0ddb3de8b368837a0d8fd95
ahmadfebri14 <ahmadfebri14@gmail.com>
2020-01-25 21:52:41 +0700

    test

--- add.php
+++ add.php
@@ -1,28 +1,13 @@
 <?php
-	include 'cennection.php';
+	include 'connection.php';
 
 	if($_GET['act'] == "add"){
 		/*Insert data.*/  
-        $insertSql = "INSERT INTO empTable (id, name, country, city, email)   
-					VALUES (?,?,?,?)";  
-        $params = array("06", &$_POST['name'], &$_POST['country'], &$_POST['city'], &$_POST['email']  
-        );  
-        $stmt = sqlsrv_query($conn, $insertSql, $params);  
-        if ($stmt === false) {  
-            /*Handle the case of a duplicte e-mail address.*/  
-            $errors = sqlsrv_errors();  
-            if ($errors[0]['code'] == 2601)  
-                {  
-                echo "The e-mail address you entered has already been used.</br>";  
-                }  
-  
-            /*Die if other errors occurred.*/  
-              else  
-                {  
-                die(print_r($errors, true));  
-                }  
-            }else{  
-				echo '<script> alert("Data tersimpan"); window.location.href="./index.php"; </scr';
-            }    
+        $name = $_POST['name'];
+		$country = $_POST['country'];
+		$city = $_POST['city'];
+		$email = $_POST['email'];
+
+		echo $name, $country, $city, $email;
 	}
 ?>
