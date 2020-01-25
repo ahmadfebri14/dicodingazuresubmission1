e7c3be2299915139e4414e33b56a835d3d9a66b6
ahmadfebri14 <ahmadfebri14@gmail.com>
2020-01-25 21:55:57 +0700

    fix error5

--- add.php
+++ add.php
@@ -2,12 +2,35 @@
 	include 'connection.php';
 
 	if($_GET['act'] == "add"){
-		/*Insert data.*/  
+	
         $name = $_POST['name'];
 		$country = $_POST['country'];
 		$city = $_POST['city'];
 		$email = $_POST['email'];
 
-		echo $name, $country, $city, $email;
-	}
+		$insertSql = "INSERT INTO empTable (name,country,city,email)   
+VALUES (?,?,?,?)";  
+        $params = array(&$name, &$country, &$city, &$email  
+        );  
+        $stmt = sqlsrv_query($conn, $insertSql, $params);  
+        if ($stmt === false)  
+            {  
+            /*Handle the case of a duplicte e-mail address.*/  
+            $errors = sqlsrv_errors();  
+            if ($errors[0]['code'] == 2601)  
+                {  
+                echo "The e-mail address you entered has already been used.</br>";  
+                }  
+  
+            /*Die if other errors occurred.*/  
+              else  
+                {  
+                die(print_r($errors, true));  
+                }  
+            }  
+          else  
+            {  
+            echo "Registration complete.</br>";  
+            }  
+        }  
 ?>
