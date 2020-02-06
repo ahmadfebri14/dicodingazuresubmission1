16f0b560f467577e43be6189f8b35009c70fad77
ahmadfebri14 <ahmadfebri14@gmail.com>
2020-02-06 22:34:49 +0700

    no message

--- kirim.php
+++ kirim.php
@@ -1,33 +1,6 @@
 <body> 
 	<?php include 'head.php' ?>
 
-<?php
-    //Mengatur instance dari Azure::Storage::Client -->
-    $connectionString = "DefaultEndpointsProtocol=https;AccountName=".getenv('febriwebapp').";AccountKey=".getenv('fyDYKm7G7lA14EsLvCh6NNGT5tXK4qZMdXEsMVlr5Apm8GA5CxoZa1b3ITnTH79CI3i5k1qVPiZh8u9p+teSqA==');
- 
-    //Membuat blob client.-->
-    $blobClient = BlobRestProxy::createBlobService($connectionString);
- 
-    # Membuat BlobService yang merepresentasikan Blob service untuk storage account
-    $createContainerOptions = new CreateContainerOptions();
- 
-    $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
- 
-    //Menetapkan metadata dari container. -->
-    $createContainerOptions->addMetaData("key1", "fyDYKm7G7lA14EsLvCh6NNGT5tXK4qZMdXEsMVlr5Apm8GA5CxoZa1b3ITnTH79CI3i5k1qVPiZh8u9p+teSqA==");
-    $createContainerOptions->addMetaData("key2", "FXv8NrMzTaZ23FyVJ0EgmNCDvUM+gRykvdot/vqvoK1tVRs20m/mvZPIFg0VX3MAmO8kmktyd6isU64CgdxArA==");
- 
-    $containerName = "blockblobs"."generateRandomString()";
- 
-        // Membuat container.
-        $blobClient->createContainer("container_febri", $createContainerOptions);
-
-    
-    //Sampai kode di atas kita telah membuat instancce Azure storage client, menginstansiasi objek blob service, membuat container baru, dan mengatur perijinan ke container agar blob bisa diakses oleh semua.
-    ?>
-
-<div class="container">
-
 <div class="container">
     <form action="./add.php?act=add" method="POST">
 		  <div class="form-group row">
