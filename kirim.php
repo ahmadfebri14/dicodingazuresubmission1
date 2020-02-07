6ae84db312a7a48237ddbea82797ab5b58ce93a3
ahmadfebri14 <ahmadfebri14@gmail.com>
2020-02-08 05:24:48 +0700

    no message

--- kirim.php
+++ kirim.php
@@ -9,7 +9,7 @@
 			<br>
 		  	<div class="form-group row">
 				<div class="col-sm-10">
-					<input type="file" id="file" name="file" class="form-control" style="padding:10px;" >
+					<input type="file" id="file" name="file" class="form-control" style="padding:20px;" >
 				</div>
 			</div>
 		  
--- simpan_gambar.php
+++ simpan_gambar.php
@@ -1,39 +1,4 @@
-<?php include 'head.php' ?>
-<br><br>
 <?php
-/**----------------------------------------------------------------------------------
-* Microsoft Developer & Platform Evangelism
-*
-* Copyright (c) Microsoft Corporation. All rights reserved.
-*
-* THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
-* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
-* OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
-*----------------------------------------------------------------------------------
-* The example companies, organizations, products, domain names,
-* e-mail addresses, logos, people, places, and events depicted
-* herein are fictitious.  No association with any real company,
-* organization, product, domain name, email address, logo, person,
-* places, or events is intended or should be inferred.
-*----------------------------------------------------------------------------------
-**/
-
-/** -------------------------------------------------------------
-# Azure Storage Blob Sample - Demonstrate how to use the Blob Storage service. 
-# Blob storage stores unstructured data such as text, binary data, documents or media files. 
-# Blobs can be accessed from anywhere in the world via HTTP or HTTPS. 
-#
-# Documentation References: 
-#  - Associated Article - https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-php 
-#  - What is a Storage Account - http://azure.microsoft.com/en-us/documentation/articles/storage-whatis-account/ 
-#  - Getting Started with Blobs - https://azure.microsoft.com/en-us/documentation/articles/storage-php-how-to-use-blobs/
-#  - Blob Service Concepts - http://msdn.microsoft.com/en-us/library/dd179376.aspx 
-#  - Blob Service REST API - http://msdn.microsoft.com/en-us/library/dd135733.aspx 
-#  - Blob Service PHP API - https://github.com/Azure/azure-storage-php
-#  - Storage Emulator - http://azure.microsoft.com/en-us/documentation/articles/storage-use-emulator/ 
-#
-**/
-
 require_once 'vendor/autoload.php';
 require_once "./random_string.php";
 
@@ -55,27 +20,10 @@ $ekstensi = strtolower(end($x));
 $ukuran	= $_FILES['file']['size'];
 $tmp_file = $_FILES['file']['tmp_name'];
 $fileToUpload = $nama;
-// $fileToUpload = "andro.png";
 
 
-if (!isset($_GET["Cleanup"])) {
     // Create container options object.
     $createContainerOptions = new CreateContainerOptions();
-
-    // Set public access policy. Possible values are
-    // PublicAccessType::CONTAINER_AND_BLOBS and PublicAccessType::BLOBS_ONLY.
-    // CONTAINER_AND_BLOBS:
-    // Specifies full public read access for container and blob data.
-    // proxys can enumerate blobs within the container via anonymous
-    // request, but cannot enumerate containers within the storage account.
-    //
-    // BLOBS_ONLY:
-    // Specifies public read access for blobs. Blob data within this
-    // container can be read via anonymous request, but container data is not
-    // available. proxys cannot enumerate blobs within the container via
-    // anonymous request.
-    // If this value is not specified in the request, container data is
-    // private to the account owner.
     $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
 
     // Set container metadata.
@@ -85,12 +33,6 @@ if (!isset($_GET["Cleanup"])) {
     $containerName = "imagefile";
 
     try {
-        // Create container.
-        // $blobClient->createContainer($containerName, $createContainerOptions);
-
-        // Getting local file so that we can upload it to Azure
-        //$myfile = fopen($fileToUpload, "r") or die("Unable to open file!");
-        //fclose($myfile);
         
         # Upload file as a block blob
         echo "Uploading BlockBlob: ".PHP_EOL;
@@ -101,46 +43,6 @@ if (!isset($_GET["Cleanup"])) {
 
         //Upload blob
         $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
-
-        // List blobs.
-        $listBlobsOptions = new ListBlobsOptions();
-        $listBlobsOptions->setPrefix("HelloWorld");
-
-        echo "These are the blobs present in the container: ";
-
-        do{
-            $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
-            foreach ($result->getBlobs() as $blob)
-            {
-                echo $blob->getName().": ".$blob->getUrl()."<br />";
-            }
-        
-            $listBlobsOptions->setContinuationToken($result->getContinuationToken());
-        } while($result->getContinuationToken());
-        echo "<br />";
-
-        // Get blob.
-        echo "This is the content of the blob uploaded: ";
-        $blob = $blobClient->getBlob($containerName, $fileToUpload);
-        ?>
-        <?php
-            echo "<img src=";
-            echo "https://febriwebapp.blob.core.windows.net/";
-            echo $containerName;
-            echo "/";
-            echo $fileToUpload;
-        ?>
-            <!-- <img src="https://febriwebapp.blob.core.windows.net/","blockblobsjnfmnq/bendera.png" /> -->
-            <!-- https://febriwebapp.blob.core.windows.net/blockblobsiytxzk/bendera.png -->
-        <?php
-        // fpassthru($blob->getContentStream());
-
-        // $blob = $this->blobRestProxy->getBlob($this->containerName, $blob_name.'.jpg');
-        // header("Content-Type:image/jpeg");
-        // header('Content-Disposition: attachment; filename="' . $blob_name . '"');
-        // fpassthru($blob->getContentStream());
-
-        echo "<br />";
     }
     catch(ServiceException $e){
         // Handle exception based on error codes and messages.
@@ -158,29 +60,6 @@ if (!isset($_GET["Cleanup"])) {
         $error_message = $e->getMessage();
         echo $code.": ".$error_message."<br />";
     }
-} 
-else 
-{
 
-    try{
-        // Delete container.
-        echo "Deleting Container".PHP_EOL;
-        echo $_GET["containerName"].PHP_EOL;
-        echo "<br />";
-        $blobClient->deleteContainer($_GET["containerName"]);
-    }
-    catch(ServiceException $e){
-        // Handle exception based on error codes and messages.
-        // Error codes and messages are here:
-        // http://msdn.microsoft.com/library/azure/dd179439.aspx
-        $code = $e->getCode();
-        $error_message = $e->getMessage();
-        echo $code.": ".$error_message."<br />";
-    }
-}
+    echo '<script> alert("Image has been upload"); window.location.href="./view_image.php"; </script>';
 ?>
-
-
-<form method="post" action="phpQS.php?Cleanup&containerName=<?php echo $containerName; ?>">
-    <button type="submit">Press to clean up all resources created by this sample</button>
-</form>
