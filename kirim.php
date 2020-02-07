0abc65e0876a6d5b707c882ad0d0f7405a569079
ahmadfebri14 <ahmadfebri14@gmail.com>
2020-02-08 04:56:32 +0700

    no message

--- kirim.php
+++ kirim.php
@@ -21,8 +21,8 @@
 				<br>
 		  		<div class="input-group mb-3">
 					<div class="custom-file">
-						<input type="file" class="custom-file-label" id="file" name="file" placeholder="Choose Image">
-						<!-- <label class="custom-file-label" >Choose file</label> -->
+						<input type="file" class="custom-file-input" id="file" name="file">
+						<label class="custom-file-label"></label>
 					</div>
 					<div class="input-group-append">
 						<button type="submit" class="input-group-text">Upload</button>
