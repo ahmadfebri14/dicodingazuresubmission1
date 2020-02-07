336d4b4d1ebd995c9ee7ba0ec047fc2d94ea1c84
ahmadfebri14 <ahmadfebri14@gmail.com>
2020-02-07 15:52:35 +0700

    delete junk file

--- kirim.php
+++ kirim.php
@@ -2,17 +2,20 @@
 <?php include 'head.php' ?>
 <br><br>
 <div class="container">
-    <form action="./simpan_gambar.php?act=add" method="POST">
+    <!-- Content here -->
+		<form action="./phpqs.php" method="POST" enctype="multipart/form-data">
 		  <div class="form-group row">
 			<label for="inputEmail3" class="col-sm-2 col-form-label">Name</label>
 			<div class="col-sm-10">
-            <input type="file" name="file" id="file">
+			  <input type="file" class="form-control" id="file" name="file">
 			</div>
-
-            <div class="form-group row">
+		  
+		  <div class="form-group row">
 			<div class="col-sm-10">
 			  <button type="submit" class="btn btn-primary">Submit</button>
 			</div>
+		  </div>
+		</form>
 		  </div>
     </body>
 </html>
