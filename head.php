51ec6158c9e72ba68e64f20cb652c931481507f3
ahmadfebri14 <ahmadfebri14@gmail.com>
2020-02-08 05:58:24 +0700

    add analyze_image.php

--- head.php
+++ head.php
@@ -11,6 +11,9 @@
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
    
+  <!--Link ajax (For Microsoft Computer Vision) -->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+
 </head>
 <body> 
 <?php include 'connection.php'?>
@@ -36,7 +39,7 @@
           <a class="dropdown-item" href="kirim.php">Upload Image</a>
           <a class="dropdown-item" href="view_image.php">Gallery</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Analyze Image</a>
+          <a class="dropdown-item" href="analyze_image.php">Analyze Image</a>
         </div>
     </ul>
     <form class="form-inline my-2 my-lg-0">
