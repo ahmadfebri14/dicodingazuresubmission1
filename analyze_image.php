b4b2e6fb8b696b0e582d0f4637cf27f2bdd3fa44
ahmadfebri14 <ahmadfebri14@gmail.com>
2020-02-08 07:55:28 +0700

    no message

--- analyze_image.php
+++ analyze_image.php
@@ -1,11 +1,48 @@
+<?php
+header('Access-Control-Allow-Origin: *');  
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Analyze Sample</title>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    
 </head>
 <body>
- 
+<?php include 'webconfig.php'?>
+<?php include 'web.xml' ?>
+
+<?php
+function cors() {
+
+    // Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+        // you want to allow, and if so:
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            // may also be using PUT, PATCH, HEAD etc
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        exit(0);
+    }
+
+    echo "You have CORS!";
+}
+
+cors();
+?>
+
 <script type="text/javascript">
     function processImage() {
         // **********************************************
@@ -91,5 +128,6 @@ Image to analyze:
         <img id="sourceImage" width="400" />
     </div>
 </div>
+
 </body>
 </html>
