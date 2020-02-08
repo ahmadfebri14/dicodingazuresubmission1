7561a6f2a33dfb68e0b196758feb65329095be6a
ahmadfebri14 <ahmadfebri14@gmail.com>
2020-02-08 09:13:25 +0700

    no message

--- analyze_image.php
+++ analyze_image.php
@@ -10,6 +10,81 @@
 <body>
 <?php include 'head.php' ?>
 <br><br>
+
+<?php
+require_once 'vendor/autoload.php';
+require_once "./random_string.php";
+
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+
+
+//cek apakah data kosong
+if($_GET['act'] == "add"){
+    if (empty($_FILES['file']['name'])) {
+        echo '<script> alert("You haven\'t selected any pictures yet"); window.location.href="./kirim.php"; </script>';
+    } else {
+
+        $connectionString = "DefaultEndpointsProtocol=https;AccountName=febriwebapp;AccountKey=fyDYKm7G7lA14EsLvCh6NNGT5tXK4qZMdXEsMVlr5Apm8GA5CxoZa1b3ITnTH79CI3i5k1qVPiZh8u9p+teSqA==;EndpointSuffix=core.windows.net";
+        
+        // Create blob client.
+        $blobClient = BlobRestProxy::createBlobService($connectionString);
+        
+        
+        $nama = $_FILES['file']['name'];
+        $x = explode('.', $nama);
+        $ekstensi = strtolower(end($x));
+        $ukuran	= $_FILES['file']['size'];
+        $tmp_file = $_FILES['file']['tmp_name'];
+        $fileToUpload = $nama;
+        
+        
+            // Create container options object.
+            $createContainerOptions = new CreateContainerOptions();
+            $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+        
+            // Set container metadata.
+            $createContainerOptions->addMetaData("key1", "fyDYKm7G7lA14EsLvCh6NNGT5tXK4qZMdXEsMVlr5Apm8GA5CxoZa1b3ITnTH79CI3i5k1qVPiZh8u9p+teSqA==");
+            $createContainerOptions->addMetaData("key2", "DefaultEndpointsProtocol=https;AccountName=febriwebapp;AccountKey=fyDYKm7G7lA14EsLvCh6NNGT5tXK4qZMdXEsMVlr5Apm8GA5CxoZa1b3ITnTH79CI3i5k1qVPiZh8u9p+teSqA==;EndpointSuffix=core.windows.net");
+        
+            $containerName = "imagefile";
+        
+            try {
+                
+                # Upload file as a block blob
+                // echo "Uploading BlockBlob: ".PHP_EOL;
+                // echo $fileToUpload;
+                // echo "<br />";
+                
+                $content = fopen($tmp_file, "r");
+        
+                //Upload blob
+                $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+            }
+            catch(ServiceException $e){
+                // Handle exception based on error codes and messages.
+                // Error codes and messages are here:
+                // http://msdn.microsoft.com/library/azure/dd179439.aspx
+                $code = $e->getCode();
+                $error_message = $e->getMessage();
+                echo $code.": ".$error_message."<br />";
+            }
+            catch(InvalidArgumentTypeException $e){
+                // Handle exception based on error codes and messages.
+                // Error codes and messages are here:
+                // http://msdn.microsoft.com/library/azure/dd179439.aspx
+                $code = $e->getCode();
+                $error_message = $e->getMessage();
+                echo $code.": ".$error_message."<br />";
+            }
+            $url_gambar = $blob->getUrl();
+            echo $url_gambar;
+    
+?>
+
 <div class="container">
     <h1>Analyze image:</h1>
     Enter the URL to an image, then click the <strong>Analyze image</strong> button.
@@ -44,6 +119,10 @@
         </div>
 </div>
 </div> 
+<?php
+}
+}
+?>
 
 <script type="text/javascript">
     function processImage() {
