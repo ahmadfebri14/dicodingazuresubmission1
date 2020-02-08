67ecfbc5075f3cf26edc7c48ea28540172e8f440
ahmadfebri14 <ahmadfebri14@gmail.com>
2020-02-08 09:07:44 +0700

    no message

--- analyze_image.php
+++ analyze_image.php
@@ -10,81 +10,40 @@
 <body>
 <?php include 'head.php' ?>
 <br><br>
-<?php
-require_once 'vendor/autoload.php';
-require_once "./random_string.php";
-
-use MicrosoftAzure\Storage\Blob\BlobRestProxy;
-use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
-use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
-use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
-use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
-
-
-//cek apakah data kosong
-if($_GET['act'] == "add"){
-    if (empty($_FILES['file']['name'])) {
-        echo '<script> alert("You haven\'t selected any pictures yet"); window.location.href="./kirim.php"; </script>';
-    } else {
-
-        $connectionString = "DefaultEndpointsProtocol=https;AccountName=febriwebapp;AccountKey=fyDYKm7G7lA14EsLvCh6NNGT5tXK4qZMdXEsMVlr5Apm8GA5CxoZa1b3ITnTH79CI3i5k1qVPiZh8u9p+teSqA==;EndpointSuffix=core.windows.net";
-        
-        // Create blob client.
-        $blobClient = BlobRestProxy::createBlobService($connectionString);
-        
-        
-        $nama = $_FILES['file']['name'];
-        $x = explode('.', $nama);
-        $ekstensi = strtolower(end($x));
-        $ukuran	= $_FILES['file']['size'];
-        $tmp_file = $_FILES['file']['tmp_name'];
-        $fileToUpload = $nama;
-        
-        
-            // Create container options object.
-            $createContainerOptions = new CreateContainerOptions();
-            $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
-        
-            // Set container metadata.
-            $createContainerOptions->addMetaData("key1", "fyDYKm7G7lA14EsLvCh6NNGT5tXK4qZMdXEsMVlr5Apm8GA5CxoZa1b3ITnTH79CI3i5k1qVPiZh8u9p+teSqA==");
-            $createContainerOptions->addMetaData("key2", "DefaultEndpointsProtocol=https;AccountName=febriwebapp;AccountKey=fyDYKm7G7lA14EsLvCh6NNGT5tXK4qZMdXEsMVlr5Apm8GA5CxoZa1b3ITnTH79CI3i5k1qVPiZh8u9p+teSqA==;EndpointSuffix=core.windows.net");
-        
-            $containerName = "imagefile";
-        
-            try {
-                
-                # Upload file as a block blob
-                // echo "Uploading BlockBlob: ".PHP_EOL;
-                // echo $fileToUpload;
-                // echo "<br />";
-                
-                $content = fopen($tmp_file, "r");
-        
-                //Upload blob
-                $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
-            }
-            catch(ServiceException $e){
-                // Handle exception based on error codes and messages.
-                // Error codes and messages are here:
-                // http://msdn.microsoft.com/library/azure/dd179439.aspx
-                $code = $e->getCode();
-                $error_message = $e->getMessage();
-                echo $code.": ".$error_message."<br />";
-            }
-            catch(InvalidArgumentTypeException $e){
-                // Handle exception based on error codes and messages.
-                // Error codes and messages are here:
-                // http://msdn.microsoft.com/library/azure/dd179439.aspx
-                $code = $e->getCode();
-                $error_message = $e->getMessage();
-                echo $code.": ".$error_message."<br />";
-            }
-        
-            echo '<script> alert("Image has been upload"); </script>';
-            $url_gambar = $blob->getUrl();
-        
-
-?>
+<div class="container">
+    <h1>Analyze image:</h1>
+    Enter the URL to an image, then click the <strong>Analyze image</strong> button.
+    <br><br>
+    
+    <div class="form-group row">
+			<label for="inputEmail3" class="col-sm-2 col-form-label">URL</label>
+			<div class="col-sm-10">
+                <input type="text" name="inputImage" id="inputImage" class="form-control"
+                value="<?php echo $url_gambar?>"/>
+			</div>
+		  </div>
+		  
+		  <div class="form-group row">
+			<div class="col-sm-10">
+            <button onclick="processImage()" class="btn btn-primary">Analyze image</button>
+			</div>
+		  </div>
+    
+    <br><br>
+    <div id="wrapper" style="width:1020px; display:table;">
+        <div id="jsonOutput" style="width:600px; display:table-cell;">
+            Response:
+            <br><br>
+            <textarea id="responseTextArea" class="UIInput"
+                    style="width:580px; height:400px;"></textarea>
+        </div>
+        <div id="imageDiv" style="width:420px; display:table-cell;">
+            Source image:
+            <br><br>
+            <img id="sourceImage" width="400" />
+        </div>
+</div>
+</div> 
 
 <script type="text/javascript">
     function processImage() {
@@ -136,46 +95,5 @@ if($_GET['act'] == "add"){
         });
     };
 </script>
- 
-
-
-<div class="container">
-    <h1>Analyze image:</h1>
-    Enter the URL to an image, then click the <strong>Analyze image</strong> button.
-    <br><br>
-    
-    <div class="form-group row">
-			<label for="inputEmail3" class="col-sm-2 col-form-label">URL</label>
-			<div class="col-sm-10">
-                <input type="text" name="inputImage" id="inputImage" class="form-control"
-                value="<?php echo $url_gambar?>"/>
-			</div>
-		  </div>
-		  
-		  <div class="form-group row">
-			<div class="col-sm-10">
-            <button onclick="processImage()" class="btn btn-primary">Analyze image</button>
-			</div>
-		  </div>
-    
-    <br><br>
-    <div id="wrapper" style="width:1020px; display:table;">
-        <div id="jsonOutput" style="width:600px; display:table-cell;">
-            Response:
-            <br><br>
-            <textarea id="responseTextArea" class="UIInput"
-                    style="width:580px; height:400px;"></textarea>
-        </div>
-        <div id="imageDiv" style="width:420px; display:table-cell;">
-            Source image:
-            <br><br>
-            <img id="sourceImage" width="400" />
-        </div>
-</div>
-</div> 
-<?php
-}
-}
-?>
 </body>
 </html>
