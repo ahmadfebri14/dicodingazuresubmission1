0e69bd3935fbcf9445aece9908fd33041dbf3ca9
ahmadfebri14 <ahmadfebri14@gmail.com>
2020-02-08 09:31:14 +0700

    no message

--- analyze_image.php
+++ analyze_image.php
@@ -2,28 +2,20 @@
 <html>
 <head>
     <title>Analyze Sample</title>
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
+	    <!-- <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
 </head>
 <body>
- 
+<?php include 'head.php' ?>
+<br><br>
+
+
 <script type="text/javascript">
     function processImage() {
-        // **********************************************
-        // *** Update or verify the following values. ***
-        // **********************************************
- 
-        // Replace <Subscription Key> with your valid subscription key.
         var subscriptionKey = "2991b6fccca14af79420e8265a724562";
  
-        // You must use the same Azure region in your REST API method as you used to
-        // get your subscription keys. For example, if you got your subscription keys
-        // from the West US region, replace "westcentralus" in the URL
-        // below with "westus".
-        //
-        // Free trial subscription keys are generated in the "westus" region.
-        // If you use a free trial subscription key, you shouldn't need to change
-        // this region.
         var uriBase =
             "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
  
@@ -71,26 +63,40 @@
     };
 </script>
  
-<h1>Analyze image:</h1>
-Enter the URL to an image, then click the <strong>Analyze image</strong> button.
-<br><br>
-Image to analyze:
-<input type="text" name="inputImage" id="inputImage"
-    value="http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg" />
-<button onclick="processImage()">Analyze image</button>
-<br><br>
-<div id="wrapper" style="width:1020px; display:table;">
-    <div id="jsonOutput" style="width:600px; display:table-cell;">
-        Response:
-        <br><br>
-        <textarea id="responseTextArea" class="UIInput"
-                  style="width:580px; height:400px;"></textarea>
-    </div>
-    <div id="imageDiv" style="width:420px; display:table-cell;">
-        Source image:
-        <br><br>
-        <img id="sourceImage" width="400" />
-    </div>
+
+
+<div class="container">
+    <h1>Analyze image:</h1>
+    Enter the URL to an image, then click the <strong>Analyze image</strong> button.
+    <br><br>
+    
+    <div class="form-group row">
+			<label for="inputEmail3" class="col-sm-2 col-form-label">URL</label>
+			<div class="col-sm-10">
+                <input type="text" name="inputImage" id="inputImage" class="form-control"/>
+			</div>
+		  </div>
+		  
+		  <div class="form-group row">
+			<div class="col-sm-10">
+            <button onclick="processImage()" class="btn btn-primary">Analyze image</button>
+			</div>
+		  </div>
+    
+    <br><br>
+    <div id="wrapper" style="width:1020px; display:table;">
+        <div id="jsonOutput" style="width:600px; display:table-cell;">
+            Response:
+            <br><br>
+            <textarea id="responseTextArea" class="UIInput"
+                    style="width:580px; height:400px;"></textarea>
+        </div>
+        <div id="imageDiv" style="width:420px; display:table-cell;">
+            Source image:
+            <br><br>
+            <img id="sourceImage" width="400" />
+        </div>
+</div>
 </div>
 </body>
 </html>
